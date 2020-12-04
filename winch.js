5eecede8e378ae972cebe8371e17de371cfc4bf6
arama-san <66212745+arama-san@users.noreply.github.com>
2020-12-04 21:53:41 +0900

    winch 数値計算プログラム完成(12/04)

--- winch.js
+++ winch.js
@@ -1,12 +1,12 @@
 
 function clickBtn1(){    
-    // var weight = document.getElementById("weight").value;
-    // var me = document.getElementById("me").value;
-    // var wheel = document.getElementById("wheel").value;
-    // var v_y = document.getElementById("v_y").value;
+    var weight = document.getElementById("weight").value;
+    var me = document.getElementById("me").value;
+    var wheel = document.getElementById("wheel").value;
+    var v_y = document.getElementById("v_y").value;
 
-    var list = document.getElementsByClassName('input-Box');
-    //数値チェック
+    //---数値チェック---//
+    var list = document.getElementsByClassName('input-Box');    //戻り値はhtmlコレクション(配列)
     var check = 0;
     for(var i = 0; i < list.length; i++)
     {
@@ -16,10 +16,20 @@ function clickBtn1(){
         }
         else{}
     }
-    
+    //---エラー表示---//
     if(check != 0)
     {
-        alert('エラー');
+        document.getElementById("power").innerHTML = "-ERROR-";
+        document.getElementById("rpm").innerHTML = "-ERROR-";
+        document.getElementById("tor").innerHTML = "-ERROR-";
+    }
+    else{
+        var power = 9.8*weight*v_y/me;
+        var rpm = 60*v_y/(3.141*wheel);
+        var tor = 100*power/(rpm*1.027);
+        document.getElementById("power").innerHTML = power.toFixed(4);
+        document.getElementById("rpm").innerHTML = rpm.toFixed(4);
+        document.getElementById("tor").innerHTML = tor.toFixed(4);
     }
     
 }
@@ -30,7 +40,7 @@ function clickBtn2(){
     document.getElementById("wheel").value = 0;
     document.getElementById("v_y").value = 0;
     document.getElementById("power").innerHTML = 0;
-    document.getElementById("rev").innerHTML = 0;
+    document.getElementById("rpm").innerHTML = 0;
     document.getElementById("tor").innerHTML = 0;
     
 }
