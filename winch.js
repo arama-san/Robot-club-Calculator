83c333b9afb3f69c4d358224bc174902442c9b6a
arama-san <66212745+arama-san@users.noreply.github.com>
2020-12-05 07:09:25 +0900

    ウィンチ単位切り替えできるようにした。

--- winch.js
+++ winch.js
@@ -6,7 +6,7 @@ function clickBtn1(){
     var v_y = document.getElementById("v_y").value;
 
     //---数値チェック---//
-    var list = document.getElementsByClassName('input-Box');    //戻り値はhtmlコレクション(配列)
+    var list = document.getElementsByClassName('input-Box');    //戻り値はhtmlコレクション(数値ではない配列)
     var check = 0;
     for(var i = 0; i < list.length; i++)
     {
@@ -23,17 +23,23 @@ function clickBtn1(){
         document.getElementById("rpm").innerHTML = "-ERROR-";
         document.getElementById("tor").innerHTML = "-ERROR-";
     }
+    //---数値計算---//
     else{
-        var power = 9.8*weight*v_y/me;
-        var rpm = 60*v_y/(3.141*wheel);
-        var tor = 100*power/(rpm*1.027);
+        var wei_unit = document.getElementById("wei_unit").value;
+        var whe_unit = document.getElementById("whe_unit").value;
+        var v_y_unit = document.getElementById("v_y_unit").value;
+        var tor_unit = document.getElementById("tor_unit").value;
+
+        var power = 9.8*(weight*wei_unit)*(v_y*v_y_unit)/me;
+        var rpm = 60*(v_y*v_y_unit)/(3.141*wheel*whe_unit);
+        var tor = (100*power/(rpm*1.027))*tor_unit;
         document.getElementById("power").innerHTML = power.toFixed(4);
         document.getElementById("rpm").innerHTML = rpm.toFixed(4);
         document.getElementById("tor").innerHTML = tor.toFixed(4);
     }
     
 }
-
+//---リセットボタン---//
 function clickBtn2(){
     document.getElementById("weight").value = 0;
     document.getElementById("me").value = 80;
