15005a778f02f81705ba009707cd8505241b96ff
arama-san <66212745+arama-san@users.noreply.github.com>
2020-12-19 20:48:55 +0900

    効果音追加

--- winch.js
+++ winch.js
@@ -4,35 +4,32 @@ function clickBtn1(){
     var me = document.getElementById("me").value;
     var wheel = document.getElementById("wheel").value;
     var v_y = document.getElementById("v_y").value;
+    
+    //---単位組み込み---//
+    var wei_unit = document.getElementById("wei_unit").value;
+    var whe_unit = document.getElementById("whe_unit").value;
+    var v_y_unit = document.getElementById("v_y_unit").value;
+    var tor_unit = document.getElementById("tor_unit").value;
 
-    //---数値チェック---//
-    var list = document.getElementsByClassName('input-Box');    //戻り値はhtmlコレクション(数値ではない配列)
-    var check = 0;
-    for(var i = 0; i < list.length; i++)
-    {
-        var e = list[i].value;
-        if(e == 0){
-            check = 1;
-        }
-        else{}
-    }
+    //---計算---//
+    var power = 9.8*(weight*wei_unit)*(v_y*v_y_unit)/(me*0.001);
+    var rpm = 60*(v_y*v_y_unit)/(3.141*wheel*whe_unit);
+    var tor = (100*power/(rpm*1.027))*tor_unit;
+
     //---エラー表示---//
-    if(check != 0)
+    if(isFinite(power) == false || isFinite(rpm) == false || isFinite(tor) == false)
     {
-        document.getElementById("power").innerHTML = "-ERROR-";
-        document.getElementById("rpm").innerHTML = "-ERROR-";
-        document.getElementById("tor").innerHTML = "-ERROR-";
-    }
-    //---数値計算---//
-    else{
-        var wei_unit = document.getElementById("wei_unit").value;
-        var whe_unit = document.getElementById("whe_unit").value;
-        var v_y_unit = document.getElementById("v_y_unit").value;
-        var tor_unit = document.getElementById("tor_unit").value;
+        document.getElementById("error_sound").play();
+        document.getElementById("yuyu").style.color = "red";
 
-        var power = 9.8*(weight*wei_unit)*(v_y*v_y_unit)/(me*0.001);
-        var rpm = 60*(v_y*v_y_unit)/(3.141*wheel*whe_unit);
-        var tor = (100*power/(rpm*1.027))*tor_unit;
+        document.getElementById("power").innerHTML = "ERROR";
+        document.getElementById("rpm").innerHTML = "ERROR";
+        document.getElementById("tor").innerHTML = "ERROR";
+    }
+    //---計算結果表示---//
+    else
+    {
+        document.getElementById("success_sound").play();
         document.getElementById("power").innerHTML = power.toFixed(4);
         document.getElementById("rpm").innerHTML = rpm.toFixed(4);
         document.getElementById("tor").innerHTML = tor.toFixed(4);
@@ -41,6 +38,9 @@ function clickBtn1(){
 }
 //---リセットボタン---//
 function clickBtn2(){
+    document.getElementById("btnsound").play();
+    document.getElementById("yuyu").style.color = "aquamarine";
+
     document.getElementById("weight").value = 0;
     document.getElementById("me").value = 80;
     document.getElementById("wheel").value = 0;
@@ -48,5 +48,4 @@ function clickBtn2(){
     document.getElementById("power").innerHTML = 0;
     document.getElementById("rpm").innerHTML = 0;
     document.getElementById("tor").innerHTML = 0;
-    
 }
