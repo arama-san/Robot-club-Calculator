b4885687db9d44d01d3ef6160304dc3b20f85091
arama-san <66212745+arama-san@users.noreply.github.com>
2021-03-19 20:28:39 +0900

    omuni3とりま完成

--- omni3.js
+++ omni3.js
@@ -55,53 +55,55 @@ function calculateBtn(){
     wheel *= whe_unit;
     v_x *= v_x_unit;
     v_y *= v_y_unit;
-    v_t *= PI/180;
+    v_t *= PI/180;  // [°/s] → [rad/s]変換
     widthL *= widthL_unit;
-    
-    //数値計算
-    var a = (PI/4)-Math.atan(widthA/widthB);    //単位rad
-    var L = Math.sqrt((widthA/2)**2+(widthB/2)**2);  
-
-    //各ホイールのたる軸に平行な速度を算出
-    var v_1b = -v_x*Math.sin(PI/4)+v_y*Math.cos(PI/4)+L*v_t;
-    var v_2b = v_x*Math.sin(PI/4)+v_y*Math.cos(PI/4)-L*v_t;
-    var v_3b = -v_x*Math.sin(PI/4)+v_y*Math.cos(PI/4)-L*v_t;
-    var v_4b = v_x*Math.sin(PI/4)+v_y*Math.cos(PI/4)+L*v_t;
 
     //各ホイールの周速度を算出
-    var v_1 = v_1b/Math.cos(PI/4);
-    var v_2 = v_2b/Math.cos(PI/4);
-    var v_3 = v_3b/Math.cos(PI/4);
-    var v_4 = v_4b/Math.cos(PI/4);
-
+    var v_1 = -(v_x)+widthL*v_t;  
+    var v_2 = v_x*Math.cos(PI/3)-v_y*Math.sin(PI/3)+widthL*v_t;
+    var v_3 = v_x*Math.cos(PI/3)+v_y*Math.sin(PI/3)+widthL*v_t;
     //各ホイールの回転数(rpm)
     var rpm1 = 60*v_1/(PI*wheel);
     var rpm2 = 60*v_2/(PI*wheel);
     var rpm3 = 60*v_3/(PI*wheel);
-    var rpm4 = 60*v_4/(PI*wheel);
-    var rpm_total = Math.abs(rpm1)+Math.abs(rpm2)+Math.abs(rpm3)+Math.abs(rpm4);
-
+    var rpm_total = Math.abs(rpm1)+Math.abs(rpm2)+Math.abs(rpm3);
     //各ホイールの出力(回転数に応じて)
     var power_total = Math.abs((9.81*weight*(Math.sin(slope)+run_res*Math.cos(slope))*(Math.sqrt(v_x**2+v_y**2)))/me);
     var power1 = Math.abs(power_total*(rpm1/rpm_total)); 
     var power2 = Math.abs(power_total*(rpm2/rpm_total));
     var power3 = Math.abs(power_total*(rpm3/rpm_total));
-    var power4 = Math.abs(power_total*(rpm4/rpm_total));
-
     //各ホイールの負荷トルク(各ホイールの出力に応じて)
-    var tor1 = (power1*wheel*0.5*tor_unit)/v_1;
-    var tor2 = (power2*wheel*0.5*tor_unit)/v_2;
-    var tor3 = (power3*wheel*0.5*tor_unit)/v_3;
-    var tor4 = (power4*wheel*0.5*tor_unit)/v_4;
+    if(v_1==0)
+    {
+        var tor1=0;
+    }
+    else
+    {
+        var tor1 = (power1*wheel*0.5*tor_unit)/v_1;
+    }
+    if(v_2==0)
+    {
+        var tor2=0;
+    }
+    else
+    {
+        var tor2 = (power2*wheel*0.5*tor_unit)/v_2;
+    }
+    if(v_3==0)
+    {
+        var tor3=0;
+    }
+    else
+    {
+        var tor3 = (power3*wheel*0.5*tor_unit)/v_3;
+    }
 
     //最大回転数算出
-    var MAX_rpm = Math.max.apply(null, [rpm1, rpm2, rpm3, rpm4]);
-
+    var MAX_rpm = Math.max.apply(null, [Math.abs(rpm1), Math.abs(rpm2), Math.abs(rpm3)]);
     //最大出力算出
-    var MAX_power = Math.max.apply(null, [power1, power2, power3, power4]);
-
+    var MAX_power = Math.max.apply(null, [power1, power2, power3]);
     //最大トルク算出
-    var MAX_tor = Math.max(tor1, tor2, tor3, tor4);
+    var MAX_tor = Math.max.apply(null, [tor1, tor2, tor3]);
 
     //値チェック
     if(weight==0)
@@ -130,11 +132,8 @@ function calculateBtn(){
         
         if(v_t != 0 )
         {
-            if(widthA == 0){
-                document.getElementById("widthA_name").style.color = "red";
-            }
-            if(widthB == 0){
-                document.getElementById("widthB_name").style.color = "red";
+            if(widthL == 0){
+                document.getElementById("widthL_name").style.color = "red";
             }
         }
 
@@ -145,21 +144,18 @@ function calculateBtn(){
         document.getElementById("wheel1_p").innerHTML = "ERROR";
         document.getElementById("wheel2_p").innerHTML = "ERROR";
         document.getElementById("wheel3_p").innerHTML = "ERROR";
-        document.getElementById("wheel4_p").innerHTML = "ERROR";
 
         document.getElementById("wheel1_r").innerHTML = "ERROR";
         document.getElementById("wheel2_r").innerHTML = "ERROR";
         document.getElementById("wheel3_r").innerHTML = "ERROR";
-        document.getElementById("wheel4_r").innerHTML = "ERROR";
 
         document.getElementById("wheel1_t").innerHTML = "ERROR";
         document.getElementById("wheel2_t").innerHTML = "ERROR";
         document.getElementById("wheel3_t").innerHTML = "ERROR";
-        document.getElementById("wheel4_t").innerHTML = "ERROR";
     }
     else
     {
-        document.getElementById("success_sound").play();
+        document.getElementById("success_sound").play();    //成功効果音
         color_AllGreen();
 
         //計算結果表示
@@ -171,17 +167,14 @@ function calculateBtn(){
         document.getElementById("wheel1_p").innerHTML = power1.toFixed(4);
         document.getElementById("wheel2_p").innerHTML = power2.toFixed(4);
         document.getElementById("wheel3_p").innerHTML = power3.toFixed(4);
-        document.getElementById("wheel4_p").innerHTML = power4.toFixed(4);
 
         document.getElementById("wheel1_r").innerHTML = rpm1.toFixed(4);
         document.getElementById("wheel2_r").innerHTML = rpm2.toFixed(4);
         document.getElementById("wheel3_r").innerHTML = rpm3.toFixed(4);
-        document.getElementById("wheel4_r").innerHTML = rpm4.toFixed(4);
 
         document.getElementById("wheel1_t").innerHTML = tor1.toFixed(4);
         document.getElementById("wheel2_t").innerHTML = tor2.toFixed(4);
         document.getElementById("wheel3_t").innerHTML = tor3.toFixed(4);
-        document.getElementById("wheel4_t").innerHTML = tor4.toFixed(4);
     }
 }
 
@@ -227,16 +220,13 @@ function ResetBtn(){
     document.getElementById("wheel1_p").innerHTML = 0;
     document.getElementById("wheel2_p").innerHTML = 0;
     document.getElementById("wheel3_p").innerHTML = 0;
-    document.getElementById("wheel4_p").innerHTML = 0;
 
     document.getElementById("wheel1_r").innerHTML = 0;
     document.getElementById("wheel2_r").innerHTML = 0;
     document.getElementById("wheel3_r").innerHTML = 0;
-    document.getElementById("wheel4_r").innerHTML = 0;
 
     document.getElementById("wheel1_t").innerHTML = 0;
     document.getElementById("wheel2_t").innerHTML = 0;
     document.getElementById("wheel3_t").innerHTML = 0;
-    document.getElementById("wheel4_t").innerHTML = 0;
     
 }
